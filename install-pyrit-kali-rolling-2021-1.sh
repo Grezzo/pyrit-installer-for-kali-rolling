5074c641a005e69b044d8bd1c110bd9537a6205f
Graeme Robinson <graeme@grobinson.me>
2021-03-17 16:23:14 +0000

    Create install-pyrit-kali-rolling-2021-1.sh

---
+++ install-pyrit-kali-rolling-2021-1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get install -y \
+	python-dev \
+	libssl-dev \
+	libpcap-dev
+
+# Clone Repo
+git clone https://github.com/JPaulMora/Pyrit.git --depth=1
+
+# Fudge code as suggested in https://github.com/JPaulMora/Pyrit/issues/591
+sed -i "s/COMPILE_AESNI/COMPILE_AESNIX/" Pyrit/cpyrit/_cpyrit_cpu.c
+
+# Build and install
+cd Pyrit
+python setup.py clean
+python setup.py build
+sudo python setup.py install
+cd ..
+
+# Clean Up
+rm -rf Pyrit
